1dce46dda012d3f8806c1b2f752fbcb48ef8f550
igorv <igorvinson@gmail.com>
2023-01-28 21:56:48 -0500

    load much img

--- src/getImages.js
+++ src/getImages.js
@@ -2,18 +2,22 @@ import axios from 'axios';
 
 const key = '33179207-ddce9a2e97b406c17b8415771';
 
-const options = {
-  key,
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-  q: 'kids',
-};
 
-async function getImages() {
+
+async function getImages(query, loadMore) {
   try {
+    let page = 1;
+    if(loadMore){page += 1;}
     const results = await axios.get('https://pixabay.com/api/', {
-      params: options,
+      params: {
+        key,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        q: query,
+        per_page: 40,
+        page
+      },
     });
     return results.data.hits;
   } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,25 +1,45 @@
 import getImages from './getImages';
+import Notiflix from "notiflix";
 
-const notify = data => {
-  console.log(data);
-  if (data.length === 0) {
-    Notiflix.Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  }
-};
+const form = document.querySelector('.search-form');
 
-getImages().then(notify);
 
-// webformatURL - ссылка на маленькое изображение для списка карточек.
-//   largeImageURL - ссылка на большое изображение.
-//   tags - строка с описанием изображения. Подойдет для атрибута alt.
-//   likes - количество лайков.
-//   views - количество просмотров.
-//   comments - количество комментариев.
-//   downloads - количество загрузок.
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const input = document.querySelector('.search-form input');
+  const inputValue = input.value;
+  getImages(inputValue).then(data => {
+    notify(data);
+    addPhotoCards(data);
+    //create button and add to DOM
+    createLoadMoreButton();
+    addEventButton();
+  });
+})
 
-// create photo card and add to DOM
+const createLoadMoreButton = () => {
+  const loadMoreButton = document.createElement('button');
+  loadMoreButton.classList.add('load-more');
+  loadMoreButton.textContent = 'Load more';
+  const divButton = document.querySelector('.load-more-button');
+  divButton.append(loadMoreButton);
+  return loadMoreButton;
+}
+
+const loadMoreBtn = document.querySelector('.load-more');
+const addEventButton = () => {
+  loadMoreBtn.addEventListener('click', () => {
+    const input = document.querySelector('.search-form input');
+    const inputValue = input.value;
+    const loadMore = true;
+    getImages(inputValue,loadMore).then(data => {
+      addPhotoCards(data,loadMore);
+    });
+  })
+}
+
+
+// create photo card
 const createPhotoCard = card => {
   const {
     webformatURL,
@@ -37,16 +57,19 @@ const createPhotoCard = card => {
    <div class="info">
     <p class="info-item">
       <b>Likes</b>
+      <span class="counts">${likes}</span>
     </p>
     <p class="info-item">
       <b>Views</b>
-      ${views}
+      <span class="counts">${views}</span>
     </p>
     <p class="info-item">
       <b>Comments</b>
+      <span class="counts">${comments}</span>
     </p>
     <p class="info-item">
       <b>Downloads</b>
+      <span class="counts">${downloads}</span>
     </p>
   </div>
   `;
@@ -54,10 +77,22 @@ const createPhotoCard = card => {
 };
 
 // add photo cards to DOM
-const addPhotoCards = data => {
+function addPhotoCards (data,loadMore) {
   const photoCards = data.map(createPhotoCard);
   const gallery = document.querySelector('.gallery');
+  if(!loadMore){
+    gallery.innerHTML = '';
+  }
   gallery.append(...photoCards);
 };
 
-getImages().then(addPhotoCards);
+// add notification if no images
+getImages().then(notify);
+
+function notify(data){
+  if (data.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+};
